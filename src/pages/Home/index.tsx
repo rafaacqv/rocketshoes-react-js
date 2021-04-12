9cc3314bac08c20c4c92b469cf3833666d46c51e
Rafael Araujo <rafaacq@gmail.com>
2021-04-12 18:00:11 -0300

    Fix UI numbers

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,9 +25,12 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    return {
+      ...sumAmount,
+      [product.id]: product.amount
+    }
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -45,7 +48,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
@@ -64,7 +67,7 @@ const Home = (): JSX.Element => {
             >
               <div data-testid="cart-product-quantity">
                 <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
+                {cartItemsAmount[product.id] || 0} 
               </div>
               
               <span>ADICIONAR AO CARRINHO</span>
